7899458e2bbeffa1642f9322ab6b44dbdcebb1a3
Amanda Vilela <amandavilelaalmeida@gmail.com>
2016-10-27 17:21:50 -0200

    Optimizations to get 100points at Google PageSpeed Insights (#10)
    * Includes minify module on scss task
    
    * Includes minified css
    
    * Include a function to minify js files
    
    * Includes minified js on index.html
    
    * Includes a function to minify images
    
    * Includes minified images
    
    * Updates the date of the next meetup
    
    * Removes redundant line of scss task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
-var gulp = require('gulp');
-    sass = require('gulp-sass');
-    browserSync = require('browser-sync').create();
-    joinPaths = require('path').join;
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    joinPaths = require('path').join,
+    rename = require('gulp-rename'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin');
 
 var paths = {
   entry: 'index.html',
   scss: './scss',
+  js: './js',
+  img: './img',
   bower: './bower_components',
   dist: './dist'
 }
@@ -18,14 +24,27 @@ gulp.task('scss', function() {
         joinPaths(paths.bower, 'normalize-css')
       ]
     }).on('error', sass.logError))
+    .pipe(minifyCss())
+    .pipe(rename('style.min.css'))
     .pipe(gulp.dest(joinPaths(paths.dist, 'css')));
 });
 
-gulp.task('default', ['scss'], function() {
+gulp.task('compressJs', function () {
+    gulp.src(joinPaths(paths.js, 'scripts.js'))
+      .pipe(uglify())
+      .pipe(rename('scripts.min.js'))
+      .pipe(gulp.dest(joinPaths(paths.dist, 'js')));
+});
+
+gulp.task('compressImages', function () {
+    gulp.src(joinPaths(paths.img, '/*'))
+      .pipe(imagemin())
+      .pipe(gulp.dest(joinPaths(paths.dist, 'img')));
+});
+
+gulp.task('default', ['scss', 'compressJs', 'compressImages'], function() {
   browserSync.init({ server: './' });
-
   gulp.watch(joinPaths(paths.scss, '*.scss'), ['scss']);
-
   gulp.watch([
     paths.entry,
     joinPaths(paths.dist, '**/*.*')
